342a9860daa5e472467321efc6c2bca0409bb66e
vinayaktrivedi <vinayakt@iitk.ac.in>
2019-09-28 19:49:10 +0530

    changed script

--- Task-2a/count.sh
+++ Task-2a/count.sh
@@ -10,27 +10,29 @@ then
     exit -1
 fi
 
-
-times=$(( 350 ))
-echo "Times = ${times}"
-
-(taskset 0x4 ./spy ${times} > ./temp) & (sleep 0.001 && taskset 0x1  ${GPG_BINARY} -d ${ENCYPTED_FILE} 2>&1 > /dev/null)
-
-for job in `jobs -p`
+for i in `seq 1`
 do
-    wait $job
+    # times=$(( i*10 ))
+    times=$(( 350 ))
+    echo "Times = ${times}"
+
+    (taskset 0x4 ./spy ${times} > ./temp) & (sleep 0.001 && taskset 0x1  ${GPG_BINARY} -d ${ENCYPTED_FILE} 2>&1 > /dev/null)
+
+    for job in `jobs -p`
+    do
+        wait $job
+    done
+    S=$(cat './temp' | head -2 | grep -o 'S' | wc -l)
+    M=$(cat './temp' | head -2 | grep -o 'M' | wc -l)
+    r=$(cat './temp' | head -2 | grep -o 'r' | wc -l)
+
+    S_orig=$(cat './temp2' | grep -o 'S' | wc -l)
+    M_orig=$(cat './temp2' | grep -o 'M' | wc -l)
+    r_orig=$(cat './temp2' | grep -o 'r' | wc -l)
+    echo "S = ${S}, Orig = ${S_orig}"
+    echo "M = ${M}, Orig = ${M_orig}"
+    echo "r = ${r}, Orig = ${r_orig}"
+    echo `cat temp | head -n2 | wc -c`
+    echo ""
+    sleep 1
 done
-S=$(cat './temp' | head -2 | grep -o 'S' | wc -l)
-M=$(cat './temp' | head -2 | grep -o 'M' | wc -l)
-r=$(cat './temp' | head -2 | grep -o 'r' | wc -l)
-
-S_orig=$(cat './temp2' | grep -o 'S' | wc -l)
-M_orig=$(cat './temp2' | grep -o 'M' | wc -l)
-r_orig=$(cat './temp2' | grep -o 'r' | wc -l)
-echo "S = ${S}, Orig = ${S_orig}"
-echo "M = ${M}, Orig = ${M_orig}"
-echo "r = ${r}, Orig = ${r_orig}"
-echo `cat temp | head -n2 | wc -c`
-echo ""
-sleep 1
-
